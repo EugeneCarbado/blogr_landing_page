b4b196f2fa6431b324b970a3aec3ea6cb4ddd3ab
eugene <eugenecarbado@gmail.com>
2021-04-17 21:43:10 +0100

    .

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import "../css/header.scss";
 import "../css/main-1.scss";
 import "../css/main-2.scss";
 import "../css/main-3.scss";
+import "../css/footer.scss";
 import "../css/reset.scss";
 import "../css/colours.scss";
