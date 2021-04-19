5aa827b1a37d98e04bfbbf3b0ee99270013092e2
eugene <eugenecarbado@gmail.com>
2021-04-19 16:15:01 +0100

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 const subItem = document.getElementsByName("subItem");
+const navItem = document.getElementsByClassName("nav_head_title");
 
 
 document.getElementById('hamburger').addEventListener('click', showNav);
